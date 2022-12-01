0d3e9988fecc1c6f6c345bfc7e78f2a2c125371e
Midori Harahap <118705697+midoistory@users.noreply.github.com>
2022-12-01 09:24:22 +0700

    welcome

--- kelompok16.js
+++ kelompok16.js
@@ -5,8 +5,9 @@
 
 const input = require('readline-sync');
 console.log("----------");
+console.log("Selamat Datang di Kalkulator!");
+console.log("----------");
 console.log("+", "-", "*", "/", "%");
 var operasi = input.question("pilih salah satu operasi = ");
-console.log("----------");
 var masukannilai1 = input.question('masukkan nilai 1 = ');
 var masukannilai2 = input.question('masukkan nilai 2 = ');
